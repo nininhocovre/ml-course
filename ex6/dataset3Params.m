c7b9451246c1795ed10a3f2e0c394c2220fddb47
Marcio Renato Covre <nininhocovre@gmail.com>
2021-01-09 14:16:19 -0300

    exercises 6 complete

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,11 +23,30 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C_options = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigma_options = [0.01 0.03 0.1 0.3 1 3 10 30];
+C_min = 0;
+sigma_min = 0;
+error_min = 0;
 
+for C_val = C_options
+  for sigma_val = sigma_options
+    # train model with C and sigma values
+    model= svmTrain(X, y, C_val, @(x1, x2) gaussianKernel(x1, x2, sigma_val));
+    # run on Xval
+    y_predict = svmPredict(model, Xval);
 
+    error_calc = sum((yval - y_predict).^2);
+    if (error_min == 0 || error_calc < error_min)
+      error_min = error_calc;
+      C_min = C_val;
+      sigma_min = sigma_val;
+    endif
+  endfor
+endfor
 
-
-
+C = C_min;
+sigma = sigma_min;
 
 % =========================================================================
 
